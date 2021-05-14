95b147280757b9627be70798ffbe4e29f60f9eb7
Rachael Skyner <reskyner@googlemail.com>
2021-05-14 09:21:26 +0100

    Add github action setup for django db

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -19,6 +19,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 # Application definition
 INSTALLED_APPS = [
     "django_auth",
